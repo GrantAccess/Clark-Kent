9106a5b3ab5ed57d49f1d200b300eef5a6de4ea7
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-22 12:36:36 +0000

    the license stuff

--- superman.bat
+++ superman.bat
@@ -1,14 +1,17 @@
 @echo off
 
+:: Routine of superman.bat
+:: a batch file to create an Administrator called superman with a password clarkent
+:: by <https://github.com/GrantAccess/Clark-Kent/> (CC)
+:: the author has not changed this document since Saturday 22 Jan 2022
+
 :: turn default Administrator account on/off
 :: net user Administrator /active:yes
 :: net user Administrator /active:no
 
 :: When you have elevated privileges on a windows system you can create other users like this.
 
-:: a batch file to create an Administrator called superman with a password clarkent 
-
-:: make the account Superman & password clarkent 
+:: make the account Superman & password clarkent
 net user superman clarkent /add
 
 :: make Superman an Administrator
