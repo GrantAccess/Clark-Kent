fc57667d7d0e633a89758872c48d5d0d1f2b0b7f
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2023-03-31 16:14:12 +0100

    moved source

--- drop.bat
+++ drop.bat
@@ -1,3 +1,5 @@
 rem if a app can't be run without admin password, drag and drop to this bat.
 
 cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %1"
+
+call superman.bat
--- run_mmc.bat
+++ run_mmc.bat
@@ -2,7 +2,10 @@
 rem app may be started without invoking UAC
 rem <https://bobcares.com/blog/run-program-without-admin-privileges/>
 
-Set ApplicationPath="C:\Users\mitch\Desktop\Netplwiz.exe"
+Set ApplicationPath="C:\Windows\System32\mmc.exe"
 cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %ApplicationPath%"
 
-rem however, privileges are not raised.
+:: however, privileges are not raised.
+
+call superman.bat
+
